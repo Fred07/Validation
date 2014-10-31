d7424b4d3a38ee1196665bf688797c27db22cc0a
Fred07 <fivesec10@gmail.com>
2014-10-31 19:06:49 +0800

    更新

--- Validation.php
+++ Validation.php
@@ -8,19 +8,44 @@
  */
 
 // 直接擴充驗證區function, 遵守命名規則(checker_), 就可以直接使用
+//Todo: error_rule 暫時拿掉功能 實作流程無法確定
+//Todo: setMode 中途改變該怎麼處理
 class Validation {
 
-    private $rule_queue = array();      // 要驗證的任務排列
+    private $rule_queue;        // 要驗證的任務排列
+    private $error_code;        // 驗證失敗的錯誤碼
+    private $error_rule;        // 驗證失敗的驗證名稱    ex.'minLen' , 表示未通過最小值檢驗
+    private $valid_flag;        // 驗證狀態, true 正常
 
-    private $error_code = '';           // 驗證失敗的錯誤碼
-    private $error_rule = '';           // 驗證失敗的驗證名稱  ex. 'minLen' , 表示未通過最小值檢驗
-
-    //public $stepMode = true;    //testing
+    public $stepMode;               // 逐項檢查模式: true遇到錯誤就中止檢查, false全部rule檢查
 
     // constructor
     function Validation() {
-
+        $this->initialize();
     }
+
+    // Restart Validation Task
+    public function initialize() {
+
+        // 新開始一個驗證需要作的事情
+        // 清空rule_queue
+        // 清空error_code
+        // 清空error_rule
+        // 回復valid_flag
+
+        $this->rule_queue = array();
+        $this->error_code = array();
+        $this->error_rule = array();
+        $this->valid_flag = true;
+        $this->stepMode = true;
+    }
+
+    // not yet, 改變模式
+    /*public function stepMode( $mode ) {
+
+        $this->stepMode = (( $mode )?true:false);
+        $this->initialize();    // 改變模式要初始化object
+    }*/
 
     // Getter
     public function getErrorCode() {
@@ -32,6 +57,20 @@ class Validation {
         return $this->error_rule;
     }
 
+    // not yet!! 最後輸出的格式
+    // Get error_code and error_rule in format
+    public function getReadableError() {
+        if ( !$this->valid_flag ) {
+            $result = array();
+            foreach( $this->error_code AS $index => $err_code ) {
+                array_push($result,  'error_code: ' . $err_code);
+            }
+            return $result;
+        } else {
+            //no error, do nothing!!
+        }
+    }
+
     //將檢查項目加入到 rule_queue
     public function check ( $rules = array(), $value, $error ) {
 
@@ -40,28 +79,26 @@ class Validation {
 
     public function run() {
 
-        try {
+        foreach( $this->rule_queue AS $bundle ) {   //每一組check指令
 
-            foreach( $this->rule_queue AS $bundle ) {   //每一組check指令
+            $value = $bundle['value'];
+            $error = $bundle['error'];
+            foreach( $bundle['rules'] AS $rule ) {  //每一個rule 驗證項目
 
-                $value = $bundle['value'];
-                $error = $bundle['error'];
-                foreach( $bundle['rules'] AS $rule ) {  //每一個rule 驗證項目
-
-                    // 驗證失敗就中止驗證
-                    if ( !$this->switchToChecker($rule, $value) ) {
-                        $this->error_code = $error;
+                // 驗證沒過
+                if ( !$this->switchToChecker($rule, $value) ) {
+                    array_push($this->error_code, $error);
+                    $this->valid_flag = false;
+                    if ($this->stepMode) {
                         return $error;
                     }
                 }
             }
-            return true;    //全部驗證成功
-
-        } catch ( Exception $e ) {
-            return $e->getMessage();
         }
+        return $this->valid_flag;
     }
 
+    // 分配器
     // 呼叫對應的驗證function
     private function switchToChecker( $rule, $value )
     {
@@ -71,12 +108,14 @@ class Validation {
         $method = 'checker_' . $rule_couple[0];
 
         if ( method_exists(__CLASS__, $method) ) {
-            $this->error_rule = $rule_couple[0];
+            //array_push($this->error_rule, $rule_couple[0]);
             return $this->$method( $value, $rule_couple );
         } else {
-            throw new Exception('can not find function: ' . $method);
+            throw new Exception('can not find validation method: ' . $rule_couple[0]);
         }
     }
+
+
 
     // 以下各種驗證模式
     // 可繼承類別擴充新的驗證方法
@@ -85,11 +124,11 @@ class Validation {
     // @param $param array: 參數包裹, 依照所需取得
     // @return boolean: 是否正確, true:正確
 
-    public function checker_required( $value, $param ) {
+    protected function checker_required( $value, $param ) {
         return ( is_null($value) )?false:true;
     }
 
-    public function checker_minLen( $value, $param ) {
+    protected function checker_minLen( $value, $param ) {
         if (!isset($param[1])) {
             throw new Exception('missing parameter');
         }
@@ -97,7 +136,7 @@ class Validation {
         return ( mb_strlen($value, 'utf8') >= $min )?true:false;
     }
 
-    public function checker_maxLen( $value, $param ) {
+    protected function checker_maxLen( $value, $param ) {
         if (!isset($param[1])) {
             throw new Exception('missing parameter');
         }
@@ -105,21 +144,21 @@ class Validation {
         return ( mb_strlen($value, 'utf8') <= $max )?true:false;
     }
 
-    public function checker_dateFormat( $date, $param ) {
+    protected function checker_dateFormat( $date, $param ) {
         //return ( preg_match('/^\d{4}\/\d{2}\/\d{2}$/', $date) )?true:false;
         return ( preg_match('/^\d{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])$/', $date) )?true:false;
     }
 
-    public function checker_timeFormat( $time, $param ) {
+    protected function checker_timeFormat( $time, $param ) {
         //return ( preg_match('/^\d{2}:\d{2}:\d{2}$/', $time) )?true:false;
         return ( preg_match('/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/', $time) )?true:false;
     }
 
-    public function checker_dateTimeFormat( $dateTime, $param ) {
+    protected function checker_dateTimeFormat( $dateTime, $param ) {
         return ( preg_match('/^\d{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])\s(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/', $dateTime) )?true:false;
     }
 
-    public function checker_isNumeric( $value, $param ) {
+    protected function checker_isNumeric( $value, $param ) {
         return ( is_numeric($value) )?true:false;
     }
 
