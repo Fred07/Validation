89de69282f95371403d909f300132e7d3ce9054d
Fred07 <fivesec10@gmail.com>
2014-10-31 16:48:30 +0800

    更新
    配對驗證方法修正
    自動尋找對應名稱function
    修改checker: dateFormat  & timeFormat 更加精準
    增加dateTimeFormat

--- Validation.php
+++ Validation.php
@@ -7,7 +7,7 @@
  * Time: 上午 10:28
  */
 
-// 目前架構: 遇到第一個error就終止check
+// 直接擴充驗證區function, 遵守命名規則(checker_), 就可以直接使用
 class Validation {
 
     private $rule_queue = array();      // 要驗證的任務排列
@@ -15,9 +15,21 @@ class Validation {
     private $error_code = '';           // 驗證失敗的錯誤碼
     private $error_rule = '';           // 驗證失敗的驗證名稱  ex. 'minLen' , 表示未通過最小值檢驗
 
+    //public $stepMode = true;    //testing
+
     // constructor
     function Validation() {
 
+    }
+
+    // Getter
+    public function getErrorCode() {
+        return $this->error_code;
+    }
+
+    // Getter
+    public function getErrorRule() {
+        return $this->error_rule;
     }
 
     //將檢查項目加入到 rule_queue
@@ -28,94 +40,90 @@ class Validation {
 
     public function run() {
 
-        foreach( $this->rule_queue AS $bundle ) {   //每一組check指令
+        try {
 
-            $value = $bundle['value'];
-            $error = $bundle['error'];
-            foreach( $bundle['rules'] AS $rule ) {  //每一個rule 驗證項目
+            foreach( $this->rule_queue AS $bundle ) {   //每一組check指令
 
-                // 驗證失敗就中止驗證
-                if ( !$this->switchToChecker($rule, $value) ) {
-                    $this->error_code = $error;
-                    return $this->error_code;
+                $value = $bundle['value'];
+                $error = $bundle['error'];
+                foreach( $bundle['rules'] AS $rule ) {  //每一個rule 驗證項目
+
+                    // 驗證失敗就中止驗證
+                    if ( !$this->switchToChecker($rule, $value) ) {
+                        $this->error_code = $error;
+                        return $error;
+                    }
                 }
             }
+            return true;    //全部驗證成功
+
+        } catch ( Exception $e ) {
+            return $e->getMessage();
         }
-        return true;    //全部驗證成功
     }
 
-    // 配對驗證規則到對應的驗證function
+    // 呼叫對應的驗證function
     private function switchToChecker( $rule, $value )
     {
-        // 某些特殊驗證由':' 來區格規則和值. ex. min_len:5
-        // 切割出規則和驗證值
+        // 某些特殊驗證由 ':' 來區格驗證規則和驗證值. ex. min_len:5
+        // 切割出規則和驗證值, $rule_couple[0]:驗證規則,  $rule_couple[1]:驗證值, etc.
         $rule_couple = explode(':', $rule);
+        $method = 'checker_' . $rule_couple[0];
 
-        switch ($rule_couple[0]) {
-            case 'required':
-                $this->error_rule = 'required';
-                return $this->checker_required($value);
-                break;
-            case 'minLen':
-                if ( isset($rule_couple[1]) ) {
-                    $this->error_rule = 'minLen';
-                    return $this->checker_minLen($value, $rule_couple[1]);
-                }
-                break;
-            case 'maxLen':
-                if ( isset($rule_couple[1]) ) {
-                    $this->error_rule = 'maxLen';
-                    return $this->checker_maxLen($value, $rule_couple[1]);
-                }
-                break;
-            case 'dateFormat':
-                $this->error_rule = 'dateFormat';
-                return $this->checker_dateFormat($value);
-                break;
-            case 'timeFormat':
-                $this->error_rule = 'timeFormat';
-                return $this->checker_timeFormat($value);
-                break;
-            case 'isNumeric':
-                $this->error_rule = 'isNumeric';
-                return $this->checker_isNumeric($value);
-                break;
-            case 'isAlphaNumeric':
-                $this->error_rule = 'isAlphaNumeric';
-                return $this->checker_isAlphaNumeric($value);
-                break;
-            default:
-                break;
+        if ( method_exists(__CLASS__, $method) ) {
+            $this->error_rule = $rule_couple[0];
+            return $this->$method( $value, $rule_couple );
+        } else {
+            throw new Exception('can not find function: ' . $method);
         }
     }
 
-    /*各種驗證模式*/
+    // 以下各種驗證模式
+    // 可繼承類別擴充新的驗證方法
+    // checker_ 開頭
+    // @param $value: 將被驗證的值
+    // @param $param array: 參數包裹, 依照所需取得
+    // @return boolean: 是否正確, true:正確
 
-    public function checker_required( $value ) {
-        return ( !empty($value) )?true:false;
+    public function checker_required( $value, $param ) {
+        return ( is_null($value) )?false:true;
     }
 
-    public function checker_minLen( $value, $min ) {
+    public function checker_minLen( $value, $param ) {
+        if (!isset($param[1])) {
+            throw new Exception('missing parameter');
+        }
+        $min = $param[1];
         return ( mb_strlen($value, 'utf8') >= $min )?true:false;
     }
 
-    public function checker_maxLen( $value, $max ) {
+    public function checker_maxLen( $value, $param ) {
+        if (!isset($param[1])) {
+            throw new Exception('missing parameter');
+        }
+        $max = $param[1];
         return ( mb_strlen($value, 'utf8') <= $max )?true:false;
     }
 
-    public function checker_dateFormat( $date ) {
-        return ( preg_match('/^\d{4}\/\d{2}\/\d{2}$/', $date) )?true:false;
+    public function checker_dateFormat( $date, $param ) {
+        //return ( preg_match('/^\d{4}\/\d{2}\/\d{2}$/', $date) )?true:false;
+        return ( preg_match('/^\d{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])$/', $date) )?true:false;
     }
 
-    public function checker_timeFormat( $time ) {
-        return ( preg_match('/^\d{2}:\d{2}:\d{2}$/', $time) )?true:false;
+    public function checker_timeFormat( $time, $param ) {
+        //return ( preg_match('/^\d{2}:\d{2}:\d{2}$/', $time) )?true:false;
+        return ( preg_match('/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/', $time) )?true:false;
     }
 
-    public function checker_isNumeric( $value ) {
+    public function checker_dateTimeFormat( $dateTime, $param ) {
+        return ( preg_match('/^\d{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])\s(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/', $dateTime) )?true:false;
+    }
+
+    public function checker_isNumeric( $value, $param ) {
         return ( is_numeric($value) )?true:false;
     }
 
-    public function checker_isAlphaNumeric( $value ) {
+    public function checker_isAlphaNumeric( $value, $param ) {
         return ( ctype_alnum($value) )?true:false;
     }
 }
