7e5002fd43004c79e08761c19fcde10aa5051913
Fred07 <fivesec10@gmail.com>
2014-11-03 17:35:29 +0800

    更新
    修正部分功能
    修正註解

--- Validation.php
+++ Validation.php
@@ -16,8 +16,9 @@ class Validation {
     private $error_code;        // 驗證失敗的錯誤碼
     private $error_rule;        // 驗證失敗的驗證名稱    ex.'minLen' , 表示未通過最小值檢驗
     private $valid_flag;        // 驗證狀態, true 正常
+    private $stepMode;          // 逐項檢查模式: true遇到錯誤就中止檢查, false全部rule檢查
 
-    public $stepMode;               // 逐項檢查模式: true遇到錯誤就中止檢查, false全部rule檢查
+    const WARN_MISSING_PARAMETER = 'missing parameter';
 
     // constructor
     function Validation() {
@@ -40,15 +41,16 @@ class Validation {
         $this->stepMode = true;
     }
 
-    // not yet, 改變模式
-    /*public function stepMode( $mode ) {
-
-        $this->stepMode = (( $mode )?true:false);
-        $this->initialize();    // 改變模式要初始化object
-    }*/
+    // 改變模式
+    public function stepMode( $bol ) {
+        $this->stepMode = (( $bol )?true:false);
+    }
 
     // Getter
-    public function getErrorCode() {
+    public function getErrorCode( $index = '' ) {
+        if ( $index AND isset($this->error_code[$index]) ) {
+            return $this->error_code[$index];
+        }
         return $this->error_code;
     }
 
@@ -71,21 +73,23 @@ class Validation {
         }
     }
 
-    //將檢查項目加入到 rule_queue
+    // 加入檢查規則項目到 rule_queue
     public function check ( $rules = array(), $value, $error ) {
 
         array_push($this->rule_queue, array('rules'=>$rules, 'value'=>$value, 'error'=>$error) );
     }
 
+    // 執行檢查
     public function run() {
 
-        foreach( $this->rule_queue AS $bundle ) {   //每一組check指令
+        foreach( $this->rule_queue AS $bundle ) {   //每一組 check指令
 
             $value = $bundle['value'];
             $error = $bundle['error'];
-            foreach( $bundle['rules'] AS $rule ) {  //每一個rule 驗證項目
+            foreach( $bundle['rules'] AS $rule ) {  //每一個 rule驗證項目
 
-                // 驗證沒過
+                // 驗證沒過, 寫入error_code
+                // stepMode 模式下, 直接返回
                 if ( !$this->switchToChecker($rule, $value) ) {
                     array_push($this->error_code, $error);
                     $this->valid_flag = false;
@@ -102,13 +106,13 @@ class Validation {
     // 呼叫對應的驗證function
     private function switchToChecker( $rule, $value )
     {
-        // 某些特殊驗證由 ':' 來區格驗證規則和驗證值. ex. min_len:5
-        // 切割出規則和驗證值, $rule_couple[0]:驗證規則,  $rule_couple[1]:驗證值, etc.
+        // 某些特殊驗證由 ':' 來區格驗證規則和驗證值. ex. minLen:5
+        // 切割出規則和驗證值, $rule_couple[0]:驗證規則,  $rule_couple[1]:驗證值, 類推.
         $rule_couple = explode(':', $rule);
         $method = 'checker_' . $rule_couple[0];
 
+        // 呼叫前確認方法存在
         if ( method_exists(__CLASS__, $method) ) {
-            //array_push($this->error_rule, $rule_couple[0]);
             return $this->$method( $value, $rule_couple );
         } else {
             throw new Exception('can not find validation method: ' . $rule_couple[0]);
@@ -116,32 +120,51 @@ class Validation {
     }
 
 
-
-    // 以下各種驗證模式
+    // 各種驗證模式
     // 可繼承類別擴充新的驗證方法
     // checker_ 開頭
     // @param $value: 將被驗證的值
-    // @param $param array: 參數包裹, 依照所需取得
+    // @param $param array: 參數包裹, [0]=>驗證方法, [1]=>驗證參數1, [2]=>驗證參數2, etc...
     // @return boolean: 是否正確, true:正確
 
     protected function checker_required( $value, $param ) {
         return ( is_null($value) )?false:true;
     }
 
+    // 最小長度
     protected function checker_minLen( $value, $param ) {
         if (!isset($param[1])) {
-            throw new Exception('missing parameter');
+            throw new Exception(self::WARN_MISSING_PARAMETER);
         }
         $min = $param[1];
         return ( mb_strlen($value, 'utf8') >= $min )?true:false;
     }
 
+    // 最大長度
     protected function checker_maxLen( $value, $param ) {
         if (!isset($param[1])) {
-            throw new Exception('missing parameter');
+            throw new Exception(self::WARN_MISSING_PARAMETER);
         }
         $max = $param[1];
         return ( mb_strlen($value, 'utf8') <= $max )?true:false;
+    }
+
+    // 最小值
+    protected function checker_minNumber($value, $param) {
+        if (!isset($param[1])) {
+            throw new Exception(self::WARN_MISSING_PARAMETER);
+        }
+        $min = $param[1];
+        return ( (int)$value >= $min)?true:false;
+    }
+
+    // 最大值
+    protected function checker_maxNumber($value, $param) {
+        if (!isset($param[1])) {
+            throw new Exception(self::WARN_MISSING_PARAMETER);
+        }
+        $max = $param[1];
+        return ( (int)$value <= $max )?true:false;
     }
 
     protected function checker_dateFormat( $date, $param ) {
@@ -162,7 +185,7 @@ class Validation {
         return ( is_numeric($value) )?true:false;
     }
 
-    public function checker_isAlphaNumeric( $value, $param ) {
+    protected function checker_isAlphaNumeric( $value, $param ) {
         return ( ctype_alnum($value) )?true:false;
     }
 }
